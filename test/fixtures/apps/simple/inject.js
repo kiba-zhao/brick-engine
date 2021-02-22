33fa76db3bc0c5bd1189cb94a84a215470f15fb8
kiba.x.zhao <kiba.rain@qq.com>
2021-02-22 17:21:54 +0800

    测试simple添加inject.js　测试addins

---
+++ test/fixtures/apps/simple/inject.js
@@ -0,0 +1,11 @@
+/**
+ * @fileOverview 注入模块附加入口
+ * @name inject.js
+ * @author kiba.x.zhao <kiba.rain@qq.com>
+ * @license MIT
+ */
+'use strict';
+
+module.exports = target => {
+  return target.addins;
+};

1d3f176840ab57cbdf4231bb0570ae58a3bae1a7
kiba.x.zhao <kiba.rain@qq.com>
2021-06-25 14:52:56 +0800

    add metadata_manager.js

--- test/lib/metadata_manager.test.js
+++ test/lib/metadata_manager.test.js
@@ -6,7 +6,7 @@
  */
 
 
-import { MetadataManager,MODULE_KEY } from '../../lib/metadata_manager';
+import { MetadataManager, MODULE_KEY } from '../../lib/metadata_manager';
 import faker from 'faker';
 
 class ModelClass {}
@@ -30,72 +30,72 @@ describe('lib/metadata_manager', () => {
 
     describe('inject', () => {
 
-      it('simple', ()=>{
+      it('simple', () => {
 
-        manager.inject(faker.datatype.datetime(),()=>{});
-        manager.inject(()=>{},ModelClass);
-        manager.inject(ModelClass,{});
-        manager.inject({},faker.datatype.array());
-        manager.inject(faker.datatype.array(),undefined);
-        manager.inject(ModelClass,null);
-        manager.inject(ModelClass,faker.datatype.string());
-        manager.inject(ModelClass,faker.datatype.number());
-        manager.inject(ModelClass,faker.datatype.datetime());
-        manager.inject(ModelClass,Symbol());
-        
+        manager.inject(faker.datatype.datetime(), () => {});
+        manager.inject(() => {}, ModelClass);
+        manager.inject(ModelClass, {});
+        manager.inject({}, faker.datatype.array());
+        manager.inject(faker.datatype.array(), undefined);
+        manager.inject(ModelClass, null);
+        manager.inject(ModelClass, faker.datatype.string());
+        manager.inject(ModelClass, faker.datatype.number());
+        manager.inject(ModelClass, faker.datatype.datetime());
+        manager.inject(ModelClass, Symbol());
+
       });
 
-      it('fatal', ()=>{
+      it('fatal', () => {
 
         const WRONG_TARGET =
               `[${MODULE_KEY}] inject Error: wrong target`;
-        
-        expect(() => manager.inject(faker.datatype.string(),ModelClass)).toThrow(
+
+        expect(() => manager.inject(faker.datatype.string(), ModelClass)).toThrow(
           WRONG_TARGET
         );
-        expect(() => manager.inject(faker.datatype.number(),ModelClass)).toThrow(
+        expect(() => manager.inject(faker.datatype.number(), ModelClass)).toThrow(
           WRONG_TARGET
         );
-        expect(() => manager.inject(faker.datatype.boolean(),ModelClass)).toThrow(
+        expect(() => manager.inject(faker.datatype.boolean(), ModelClass)).toThrow(
           WRONG_TARGET
         );
-        expect(() => manager.inject(null,ModelClass)).toThrow(
+        expect(() => manager.inject(null, ModelClass)).toThrow(
           WRONG_TARGET
-        );        
-        expect(() => manager.inject(undefined,ModelClass)).toThrow(
+        );
+        expect(() => manager.inject(undefined, ModelClass)).toThrow(
           WRONG_TARGET
-        ); 
-        expect(() => manager.inject(Symbol(),ModelClass)).toThrow(
+        );
+        expect(() => manager.inject(Symbol(), ModelClass)).toThrow(
           WRONG_TARGET
-        ); 
-        
+        );
+
       });
 
     });
 
-    describe('extact',()=>{
+    describe('extact', () => {
 
-      it('simple', ()=>{
-        
-        const target = ()=>{};
+      it('simple', () => {
+
+        const target = () => {};
         const emptyRes = manager.extract(target);
         expect(emptyRes).toEqual([]);
-        
+
         const metadata = faker.datatype.json();
-        manager.inject(target,metadata);
+        manager.inject(target, metadata);
 
         const res = manager.extract(target);
-        expect(res).toEqual([metadata]);
+        expect(res).toEqual([ metadata ]);
 
         const metadataNext = faker.datatype.datetime();
-        manager.inject(target,metadataNext);
+        manager.inject(target, metadataNext);
 
         const resNext = manager.extract(target);
-        expect(resNext).toEqual([metadata,metadataNext]);
-        
+        expect(resNext).toEqual([ metadata, metadataNext ]);
+
       });
 
-      it('fatal', ()=>{
+      it('fatal', () => {
 
         const WRONG_TARGET =
               `[${MODULE_KEY}] extract Error: wrong target`;
@@ -120,7 +120,7 @@ describe('lib/metadata_manager', () => {
         );
 
       });
-      
+
     });
 
   });
